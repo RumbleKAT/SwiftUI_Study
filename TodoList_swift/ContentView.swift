3b5a9e4256ba915753ffac6f5d3902c70f01af57
RumbleKAT <reki318@naver.com>
2019-10-16 21:07:47 +0900

    Initial Commit

---
+++ TodoList_swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoList_swift
+//
+//  Created by 송명진 on 16/10/2019.
+//  Copyright © 2019 송명진. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
