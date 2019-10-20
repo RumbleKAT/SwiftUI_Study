6953abde4e0aa66df6f84eb720d408a191578f47
RumbleKAT <reki318@naver.com>
2019-10-20 20:49:28 +0900

    modifying program state

--- TodoList_swift/ContentView.swift
+++ TodoList_swift/ContentView.swift
@@ -8,15 +8,13 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var tapCount = 0
+    
     var body: some View {
-        NavigationView{
-            Form{ //one row to 10 row
-                Section{
-                    Text("Hello World")
-                }
-            }
-            .navigationBarTitle(Text("SwiftUI"),displayMode: .inline)
+        Button("Tap Count \(tapCount)"){
+            self.tapCount += 1
         }
     }
 }
